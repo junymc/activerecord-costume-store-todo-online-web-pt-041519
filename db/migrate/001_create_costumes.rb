6d28152f83650c71f0f9907c2b3c250657b34b0e
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-11 03:38:59 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.integer :size
       t.text :image_url
       t.timestamps
