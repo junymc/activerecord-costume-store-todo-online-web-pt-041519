ef7b781699caec73a6872aa41929515810f41a64
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-11 03:37:56 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,16 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.string :whether_they_are_family_friendly_or_not
+      t.integer :opening_date
+      t.integer :closing_date
+      t.string :long_description
+  end
+
 end
