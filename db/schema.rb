c46cae215520d691504fbc6c2924e09a4189574e
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-11 03:46:51 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.integer "number_of_costumes"
-    t.integer "number_of_employees"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
-    t.integer "size"
-    t.text "image_url"
+    t.float "price"
+    t.string "size"
+    t.string "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
