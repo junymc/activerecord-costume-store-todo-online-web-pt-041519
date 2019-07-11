23c6aed819b0a6bffeb760c533416f53be03794d
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-11 03:35:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,14 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
 
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.string :whether_or_not_its_still_in_business
+      t.integer :opening_time
+      t.integer :closing_time
+
 end
