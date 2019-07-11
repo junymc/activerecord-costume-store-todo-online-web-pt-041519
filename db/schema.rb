9c9118f4724a55220b1101df5dde4319ea107620
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-11 03:47:23 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
+    t.integer "number_of_costumes"
+    t.integer "number_of_employees"
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
@@ -24,22 +24,11 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.float "price"
-    t.string "size"
-    t.string "image_url"
+    t.integer "price"
+    t.integer "size"
+    t.text "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "haunted_houses", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "theme"
-    t.float "price"
-    t.boolean "family_friendly"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
-    t.string "description"
   end
 
 end
